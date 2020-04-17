afa00894109a0af4728a000425fbb45ee392a5bd
ＴＲ <public@technotter.com>
2020-04-17 22:58:25 +0900

    Create UFunc.cs

---
+++ BIDSDispX.Core/UFunc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TR.BIDSDispX.Core
+{
+  static public class UFunc
+  {
+    private const int TRUE_VALUE = 1;
+    private const int FALSE_VALUE = 0;
+
+    static public int GetDigitVal(this double value, int pos) => (int)(Math.Sqrt(value) / Math.Pow(10, pos)) % 10;
+    static public int GetDigitVal(this float value, int pos) => GetDigitVal(value, pos);
+    static public int GetDigitVal(this int value, int pos) => GetDigitVal(value, pos);
+
+    static public int ToInt32(this bool value) => value ? TRUE_VALUE : FALSE_VALUE;
+  }
+}
