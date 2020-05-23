a8a09efd65b6f40e7224da9b8d69bd29c793be8e
ＴＲ <public@technotter.com>
2020-05-23 21:26:06 +0900

    インデントのTab化

--- BIDSDispX/BIDSDispX/MainPage.xaml.cs
+++ BIDSDispX/BIDSDispX/MainPage.xaml.cs
@@ -6,47 +6,47 @@ using TR.BIDSDispX.Core;
 
 namespace TR.BIDSDispX
 {
-  [DesignTimeVisible(false)]
-  public partial class MainPage : ContentPage
-  {
-    public MainPage()
-    {
-      InitializeComponent();
-      NavigationPage.SetHasNavigationBar(this, false);
-      NavigationPage.SetTitleView(this, null);
-      Common.Start(10, true);
+	[DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+			NavigationPage.SetHasNavigationBar(this, false);
+			NavigationPage.SetTitleView(this, null);
+			Common.Start(NO_SMEM_MODE: true);
 
-      #region BinaryAS Setting
-      Common.AutoSendSetting.BasicBVE5AS = false;
-      Common.AutoSendSetting.BasicCommonAS = false;
-      Common.AutoSendSetting.BasicConstAS = false;
-      Common.AutoSendSetting.BasicHandleAS = false;
-      Common.AutoSendSetting.BasicOBVEAS = false;
-      Common.AutoSendSetting.BasicPanelAS = false;
-      Common.AutoSendSetting.BasicSoundAS = false;
-      #endregion
+			#region BinaryAS Setting
+			Common.AutoSendSetting.BasicBVE5AS = false;
+			Common.AutoSendSetting.BasicCommonAS = false;
+			Common.AutoSendSetting.BasicConstAS = false;
+			Common.AutoSendSetting.BasicHandleAS = false;
+			Common.AutoSendSetting.BasicOBVEAS = false;
+			Common.AutoSendSetting.BasicPanelAS = false;
+			Common.AutoSendSetting.BasicSoundAS = false;
+			#endregion
 
-      IBIDSsv cm = new udp();
-      cm.Connect(string.Empty);
-      Common.Add(ref cm);
-      DispCom.ViewChanged += MainPage_PageChange;
+			IBIDSsv cm = new udp();
+			cm.Connect(string.Empty);
+			Common.Add(ref cm);
+			DispCom.ViewChanged += MainPage_PageChange;
 
-      DispCom.CurrentView = new StartPage();
-    }
+			DispCom.CurrentView = new StartPage();
+		}
 
-    private void MainPage_PageChange(object sender, EventArgs e) => Content = (View)sender ?? new StartPage();
-    
+		private void MainPage_PageChange(object sender, EventArgs e) => Content = (View)sender ?? new StartPage();
+		
 
 
-    ~MainPage(){
-      DispCom.CurrentView?.OnUnloaded();
-      Common.Dispose();
-    }
+		~MainPage(){
+			DispCom.CurrentView?.OnUnloaded();
+			Common.Dispose();
+		}
 
-    protected override void OnDisappearing()
-    {
-      Common.Dispose();
-      base.OnDisappearing();
-    }
-  }
+		protected override void OnDisappearing()
+		{
+			Common.Dispose();
+			base.OnDisappearing();
+		}
+	}
 }
--- BIDSDispX/BIDSDispX/StartPage.xaml.cs
+++ BIDSDispX/BIDSDispX/StartPage.xaml.cs
@@ -9,156 +9,156 @@ using Xamarin.Forms.Xaml;
 
 namespace TR.BIDSDispX
 {
-  [XamlCompilation(XamlCompilationOptions.Compile)]
-  public partial class StartPage : ContentView, IBIDSDispX
-  {
-    private SetValue SVClas;
-    private OnlineSync OSync;
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class StartPage : ContentView, IBIDSDispX
+	{
+		private SetValue SVClas;
+		private OnlineSync OSync;
 
-    public StartPage()
-    {
-      InitializeComponent();
-      SVClas = new SetValue() { Speed = "Initial String" };
-      OSync = new OnlineSync();
-      FindDlls();
+		public StartPage()
+		{
+			InitializeComponent();
+			SVClas = new SetValue() { Speed = "Initial String" };
+			OSync = new OnlineSync();
+			FindDlls();
 
-      BindingContext = SVClas;
+			BindingContext = SVClas;
 
-      LicenseLab.Text = Properties.Resources.LicenseFile;
-    }
+			LicenseLab.Text = Properties.Resources.LicenseFile;
+		}
 
-    public void OnBSMDChanged(object sender, SMemLib.BSMDChangedEArgs e)
-    {
-      if (IsEnabled) SVClas.Speed = e.NewData.StateData.V.ToString();
-    }
+		public void OnBSMDChanged(object sender, SMemLib.BSMDChangedEArgs e)
+		{
+			if (IsEnabled) SVClas.Speed = e.NewData.StateData.V.ToString();
+		}
 
-    //private void CATS_Start(object sender, EventArgs e) => DispCom.CurrentView = new CATSDisp();
-    private void CATS_Start(object sender, EventArgs e) => DispCom.CurrentView = new SPDisp.Class1();
-    //private void CATS_Start(object sender, EventArgs e) => DispCom.CurrentView = new drivesup.RootClass();
+		//private void CATS_Start(object sender, EventArgs e) => DispCom.CurrentView = new CATSDisp();
+		private void CATS_Start(object sender, EventArgs e) => DispCom.CurrentView = new SPDisp.Class1();
+		//private void CATS_Start(object sender, EventArgs e) => DispCom.CurrentView = new drivesup.RootClass();
 
-    private string FDPath => OSync.ModsFolderName;
+		private string FDPath => OSync.ModsFolderName;
 
-    public ContentView FirstView => this;
+		public ContentView FirstView => this;
 
-    private void FindDlls()
-    {
-      SVClas.Msg = string.Empty;
-      List<string> fnames = new List<string>();
+		private void FindDlls()
+		{
+			SVClas.Msg = string.Empty;
+			List<string> fnames = new List<string>();
 
-      var mlsl = OSync.FileReadList();
-      if (mlsl != null && mlsl.Count > 0)
-        for (int i = 0; i < mlsl.Count; i++)
-          fnames.Add(mlsl[i].ModName);
+			var mlsl = OSync.FileReadList();
+			if (mlsl != null && mlsl.Count > 0)
+				for (int i = 0; i < mlsl.Count; i++)
+					fnames.Add(mlsl[i].ModName);
 
-      if (!(fnames?.Count > 0)) SVClas.Dlls = new string[] { "There are no dlls in ↓", FDPath };
-      else SVClas.Dlls = fnames?.ToArray();
-    }
+			if (!(fnames?.Count > 0)) SVClas.Dlls = new string[] { "There are no dlls in ↓", FDPath };
+			else SVClas.Dlls = fnames?.ToArray();
+		}
 
-    private void LoadLib(object sender, EventArgs e)
-    {
-      try
-      {
-        var cv = OSync.LoadLib(SVClas.SelectedDll);
-        if (cv == null) SVClas.Msg = "Can't find the selected file";
-        else DispCom.CurrentView = cv;
-      }
-      catch (FileNotFoundException)
-      {
-        SVClas.Msg = "FileNotFound : " + SVClas.SelectedDll;
-        return;
-      }
-      catch (BadImageFormatException)
-      {
-        SVClas.Msg = "Is it really dll? : " + SVClas.SelectedDll;
-        return;
-      }
-    }
+		private void LoadLib(object sender, EventArgs e)
+		{
+			try
+			{
+				var cv = OSync.LoadLib(SVClas.SelectedDll);
+				if (cv == null) SVClas.Msg = "Can't find the selected file";
+				else DispCom.CurrentView = cv;
+			}
+			catch (FileNotFoundException)
+			{
+				SVClas.Msg = "FileNotFound : " + SVClas.SelectedDll;
+				return;
+			}
+			catch (BadImageFormatException)
+			{
+				SVClas.Msg = "Is it really dll? : " + SVClas.SelectedDll;
+				return;
+			}
+		}
 
-    private void Liblist_Reflesh(object sender, EventArgs e) => FindDlls();
+		private void Liblist_Reflesh(object sender, EventArgs e) => FindDlls();
 
-    private void ShowLicense(object sender, EventArgs e) => SVClas.LicenseLabVisible = !SVClas.LicenseLabVisible;
+		private void ShowLicense(object sender, EventArgs e) => SVClas.LicenseLabVisible = !SVClas.LicenseLabVisible;
 
-    private void Add_Local_Lib(object sender, EventArgs e)
-    {
-      try
-      {
-        OSync.AddMod();
-      } catch (Exception ex)
-      {
-        SVClas.Msg = ex.Message;
-      }
-    }
+		private void Add_Local_Lib(object sender, EventArgs e)
+		{
+			try
+			{
+				OSync.AddMod();
+			} catch (Exception ex)
+			{
+				SVClas.Msg = ex.Message;
+			}
+		}
 
-    public void OnLoaded() { }
+		public void OnLoaded() { }
 
-    public void OnPanelDChanged(object sender, SMemLib.ArrayDChangedEArgs e) { }
+		public void OnPanelDChanged(object sender, SMemLib.ArrayDChangedEArgs e) { }
 
-    public void OnSoundDChanged(object sender, SMemLib.ArrayDChangedEArgs e) { }
+		public void OnSoundDChanged(object sender, SMemLib.ArrayDChangedEArgs e) { }
 
-    public void OnOpenDChanged(object sender, SMemLib.OpenDChangedEArgs e) { }
+		public void OnOpenDChanged(object sender, SMemLib.OpenDChangedEArgs e) { }
 
-    public void OnUnloaded() { }
+		public void OnUnloaded() { }
 
-    private void AppExitEv(object sender, EventArgs e) => DispCom.AppExit();
-  }
+		private void AppExitEv(object sender, EventArgs e) => DispCom.AppExit();
+	}
 
-  internal class SetValue : INotifyPropertyChanged
-  {
-    public event PropertyChangedEventHandler PropertyChanged;
+	internal class SetValue : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
 
-    private string speed;
-    private string[] dlls;
-    private string selectedDll;
-    private string msg;
+		private string speed;
+		private string[] dlls;
+		private string selectedDll;
+		private string msg;
 
-    private bool llvis = false;
+		private bool llvis = false;
 
-    public string Speed
-    {
-      get => speed;
-      set
-      {
-        speed = value;
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Speed"));
-      }
-    }
+		public string Speed
+		{
+			get => speed;
+			set
+			{
+				speed = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Speed"));
+			}
+		}
 
-    public string[] Dlls
-    {
-      get => dlls;
-      set
-      {
-        dlls = value;
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Dlls"));
-      }
-    }
-    public string SelectedDll
-    {
-      get => selectedDll;
-      set
-      {
-        selectedDll = value;
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SelectedDll"));
-      }
-    }
-    public string Msg
-    {
-      get => msg;
-      set
-      {
-        msg = value;
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Msg"));
-      }
-    }
+		public string[] Dlls
+		{
+			get => dlls;
+			set
+			{
+				dlls = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Dlls"));
+			}
+		}
+		public string SelectedDll
+		{
+			get => selectedDll;
+			set
+			{
+				selectedDll = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SelectedDll"));
+			}
+		}
+		public string Msg
+		{
+			get => msg;
+			set
+			{
+				msg = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Msg"));
+			}
+		}
 
-    public bool LicenseLabVisible
-    {
-      get => llvis;
-      set
-      {
-        llvis = value;
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("LicenseLabVisible"));
-      }
-    }
-  }
+		public bool LicenseLabVisible
+		{
+			get => llvis;
+			set
+			{
+				llvis = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("LicenseLabVisible"));
+			}
+		}
+	}
 }
