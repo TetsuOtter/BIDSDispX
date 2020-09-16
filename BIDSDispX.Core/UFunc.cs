7a48b6703e5a7bc0c7a8d45e67446b603d202164
ＴＲ <public@technotter.com>
2020-09-17 01:19:49 +0900

    Update UFunc.cs
    - UFuncクラスの名前区間をUFuncs以下に変更
    - インライン展開の推奨を明示
    - Dp単位をPx単位に変換する"Dp2Px"メソッドを実装

--- BIDSDispX.Core/UFunc.cs
+++ BIDSDispX.Core/UFunc.cs
@@ -1,16 +1,28 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
-namespace TR.BIDSDispX.Core
+using Xamarin.Essentials;
+
+namespace TR.BIDSDispX.Core.UFuncs
 {
   static public class UFunc
   {
     private const int TRUE_VALUE = 1;
     private const int FALSE_VALUE = 0;
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     static public int GetDigitVal(this double value, int pos) => (int)(Math.Sqrt(value) / Math.Pow(10, pos)) % 10;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     static public int GetDigitVal(this float value, int pos) => GetDigitVal(value, pos);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     static public int GetDigitVal(this int value, int pos) => GetDigitVal(value, pos);
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     static public int ToInt32(this bool value) => value ? TRUE_VALUE : FALSE_VALUE;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    static public double Dp2Px(this in int DPvalue) => ((double)DPvalue).Dp2Px();
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    static public double Dp2Px(this in double DPvalue) => DPvalue / DeviceDisplay.MainDisplayInfo.Density;
   }
 }
