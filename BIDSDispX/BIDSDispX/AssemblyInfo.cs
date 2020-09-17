45de8605fd6be6bc74a3c421a238030b6465ef54
ＴＲ <public@technotter.com>
2020-09-17 14:47:03 +0900

    Add IPA Fonts
    埋め込みフォントとしてIPAフォント各種を追加
    それに伴いライセンスファイルに追記

--- BIDSDispX/BIDSDispX/AssemblyInfo.cs
+++ BIDSDispX/BIDSDispX/AssemblyInfo.cs
@@ -1,3 +1,9 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("ipag.ttf", Alias = "IPA_Gothic")]
+[assembly: ExportFont("ipagp.ttf", Alias = "IPA_PGothic")]
+[assembly: ExportFont("ipam.ttf", Alias = "IPA_Mincho")]
+[assembly: ExportFont("ipamp.ttf", Alias = "IPA_PMincho")]
