7431259dc71d0fa4ac78f2b35c95f0fc8440f3b1
ＴＲ <public@technotter.com>
2020-09-19 00:47:50 +0900

    Update SettingView.xaml.cs
    変更可能なオプションを専用クラスに列挙

--- BIDSDispX/BIDSDispX/SampleView/SettingView.xaml.cs
+++ BIDSDispX/BIDSDispX/SampleView/SettingView.xaml.cs
@@ -38,13 +38,59 @@ namespace TR.BIDSDispX.SampleView
 		}
 	}
 
-	public class MeterViewSettings : INotifyPropertyChanged
+	public class MeterViewSettings : INotifyPropertyChanged, IAngleAndValMinMax
 	{
 		public event PropertyChangedEventHandler PropertyChanged;
 		private void PropCngEvInv(string s) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(s));
 
-		#region Properties
+		#region Fields
 		private double __Radius = 100;
+		private Thickness __Margin = new Thickness(10);
+		private double __MinValAngle = 2;
+		private double __MaxValAngle = 2;
+		private double __MinValue = 2;
+		private double __MaxValue = 2;
+
+		private Thickness __Needle_Shadow_Padding = new Thickness(2);
+		private double __Shadow_Opacity = 0.4;
+		private Thickness __Needle_Padding = new Thickness(0);
+		private double __Needle_Height = 10;
+		private Color __Needle1Color = Color.Black;
+		private Color __Needle2Color = Color.Red;
+		private double __Needle1Step = 0.1;
+		private double __Needle2Step = 0.1;
+		private UsableKeys.Keys __Needle1Key = UsableKeys.Keys.bc;
+		private UsableKeys.Keys __Needle2Key = UsableKeys.Keys.mr;
+		private bool __IsNeedle1Enabled = true;
+		private bool __IsNeedle2Enabled = true;
+
+		private bool __IsMarkLEnabled = true;
+		private bool __IsMarkMEnabled = true;
+		private bool __IsMarkSEnabled = true;
+		private double __MarkLHeight = 100;
+		private double __MarkMHeight = 100;
+		private double __MarkSHeight = 100;
+		private double __MarkLWidth = 100;
+		private double __MarkMWidth = 100;
+		private double __MarkSWidth = 100;
+		private int __MarkLStep = 100;
+		private int __MarkMStep = 100;
+		private int __MarkSStep = 100;
+		private Color __MarkLColor = Color.Black;
+		private Color __MarkMColor = Color.Black;
+		private Color __MarkSColor = Color.Black;
+		private Thickness __MarkLPadding = new Thickness(0);
+		private Thickness __MarkMPadding = new Thickness(0);
+		private Thickness __MarkSPadding = new Thickness(0);
+
+		private bool __IsLabelEnabled = true;
+		private double __Label_Padding = 100;
+		private double __Label_FontSize = 100;
+		private int __Label_Step = 100;
+		#endregion
+
+		#region Properties
+		#region Basic Props
 		public double Radius
 		{
 			get => __Radius;
@@ -58,63 +104,85 @@ namespace TR.BIDSDispX.SampleView
 			}
 		}
 
-		private double __MarginX = 100;
-		public double MarginX
+		public Thickness Margin
 		{
-			get => __MarginX;
+			get => __Margin;
 			set
 			{
-				if (__MarginX == value)
+				if (__Margin == value)
 					return;
 
-				__MarginX = value;
-				PropCngEvInv(nameof(MarginX));
+				__Margin = value;
+				PropCngEvInv(nameof(Margin));
 			}
 		}
 
-		private double __MarginY = 100;
-		public double MarginY
+		public double MinValAngle
 		{
-			get => __MarginY;
+			get => __MinValAngle;
 			set
 			{
-				if (__MarginY == value)
+				if (__MinValAngle == value)
 					return;
 
-				__MarginY = value;
-				PropCngEvInv(nameof(MarginY));
+				__MinValAngle = value;
+				PropCngEvInv(nameof(MinValAngle));
 			}
 		}
 
-		private double __Shadow_DstX = 2;
-		public double Shadow_DstX
+		public double MaxValAngle
 		{
-			get => __Shadow_DstX;
+			get => __MaxValAngle;
 			set
 			{
-				if (__Shadow_DstX == value)
+				if (__MaxValAngle == value)
 					return;
 
-				__Shadow_DstX = value;
-				PropCngEvInv(nameof(Shadow_DstX));
+				__MaxValAngle = value;
+				PropCngEvInv(nameof(MaxValAngle));
 			}
 		}
 
-		private double __Shadow_DstY = 2;
-		public double Shadow_DstY
+		public double MinValue
 		{
-			get => __Shadow_DstY;
+			get => __MinValue;
 			set
 			{
-				if (__Shadow_DstY == value)
+				if (__MinValue == value)
 					return;
 
-				__Shadow_DstY = value;
-				PropCngEvInv(nameof(Shadow_DstY));
+				__MinValue = value;
+				PropCngEvInv(nameof(MinValue));
 			}
 		}
 
-		private double __Shadow_Opacity = 0.4;
+		public double MaxValue
+		{
+			get => __MaxValue;
+			set
+			{
+				if (__MaxValue == value)
+					return;
+
+				__MaxValue = value;
+				PropCngEvInv(nameof(MaxValue));
+			}
+		}
+		#endregion
+		#region Needle Props
+		public Thickness Needle_Shadow_Padding
+		{
+			get => __Needle_Shadow_Padding;
+			set
+			{
+				if (__Needle_Shadow_Padding == value)
+					return;
+
+				__Needle_Shadow_Padding = value;
+				PropCngEvInv(nameof(Needle_Shadow_Padding));
+			}
+		}
+
 		public double Shadow_Opacity
 		{
 			get => __Shadow_Opacity;
@@ -128,7 +196,6 @@ namespace TR.BIDSDispX.SampleView
 			}
 		}
 
-		private Thickness __Needle_Padding = new Thickness(0);
 		public Thickness Needle_Padding
 		{
 			get => __Needle_Padding;
@@ -142,7 +209,6 @@ namespace TR.BIDSDispX.SampleView
 			}
 		}
 
-		private double __Needle_Height = 10;
 		public double Needle_Height
 		{
 			get => __Needle_Height;
@@ -156,7 +222,150 @@ namespace TR.BIDSDispX.SampleView
 			}
 		}
 
-		private double __MarkLHeight = 100;
+		public Color Needle1Color
+		{
+			get => __Needle1Color;
+			set
+			{
+				if (__Needle1Color == value)
+					return;
+
+				__Needle1Color = value;
+				PropCngEvInv(nameof(Needle1Color));
+			}
+		}
+
+		public Color Needle2Color
+		{
+			get => __Needle2Color;
+			set
+			{
+				if (__Needle2Color == value)
+					return;
+
+				__Needle2Color = value;
+				PropCngEvInv(nameof(Needle2Color));
+			}
+		}
+
+		public double Needle1Step
+		{
+			get => __Needle1Step;
+			set
+			{
+				if (__Needle1Step == value)
+					return;
+
+				__Needle1Step = value;
+				PropCngEvInv(nameof(Needle1Step));
+			}
+		}
+
+		public double Needle2Step
+		{
+			get => __Needle2Step;
+			set
+			{
+				if (__Needle2Step == value)
+					return;
+
+				__Needle2Step = value;
+				PropCngEvInv(nameof(Needle2Step));
+			}
+		}
+
+		public UsableKeys.Keys Needle1Key
+		{
+			get => __Needle1Key;
+			set
+			{
+				if (__Needle1Key == value)
+					return;
+
+				__Needle1Key = value;
+				PropCngEvInv(nameof(Needle1Key));
+			}
+		}
+
+		public UsableKeys.Keys Needle2Key
+		{
+			get => __Needle2Key;
+			set
+			{
+				if (__Needle2Key == value)
+					return;
+
+				__Needle2Key = value;
+				PropCngEvInv(nameof(Needle2Key));
+			}
+		}
+
+		public bool IsNeedle1Enabled
+		{
+			get => __IsNeedle1Enabled;
+			set
+			{
+				if (__IsNeedle1Enabled == value)
+					return;
+
+				__IsNeedle1Enabled = value;
+				PropCngEvInv(nameof(IsNeedle1Enabled));
+			}
+		}
+
+		public bool IsNeedle2Enabled
+		{
+			get => __IsNeedle2Enabled;
+			set
+			{
+				if (__IsNeedle2Enabled == value)
+					return;
+
+				__IsNeedle2Enabled = value;
+				PropCngEvInv(nameof(IsNeedle2Enabled));
+			}
+		}
+		#endregion
+		#region Mark Props
+		public bool IsMarkLEnabled
+		{
+			get => __IsMarkLEnabled;
+			set
+			{
+				if (__IsMarkLEnabled == value)
+					return;
+
+				__IsMarkLEnabled = value;
+				PropCngEvInv(nameof(IsMarkLEnabled));
+			}
+		}
+
+		public bool IsMarkMEnabled
+		{
+			get => __IsMarkMEnabled;
+			set
+			{
+				if (__IsMarkMEnabled == value)
+					return;
+
+				__IsMarkMEnabled = value;
+				PropCngEvInv(nameof(IsMarkMEnabled));
+			}
+		}
+
+		public bool IsMarkSEnabled
+		{
+			get => __IsMarkSEnabled;
+			set
+			{
+				if (__IsMarkSEnabled == value)
+					return;
+
+				__IsMarkSEnabled = value;
+				PropCngEvInv(nameof(IsMarkSEnabled));
+			}
+		}
+
 		public double MarkLHeight
 		{
 			get => __MarkLHeight;
@@ -170,7 +379,6 @@ namespace TR.BIDSDispX.SampleView
 			}
 		}
 
-		private double __MarkMHeight = 100;
 		public double MarkMHeight
 		{
 			get => __MarkMHeight;
@@ -184,7 +392,6 @@ namespace TR.BIDSDispX.SampleView
 			}
 		}
 
-		private double __MarkSHeight = 100;
 		public double MarkSHeight
 		{
 			get => __MarkSHeight;
@@ -198,7 +405,6 @@ namespace TR.BIDSDispX.SampleView
 			}
 		}
 
-		private double __MarkLWidth = 100;
 		public double MarkLWidth
 		{
 			get => __MarkLWidth;
@@ -212,7 +418,6 @@ namespace TR.BIDSDispX.SampleView
 			}
 		}
 
-		private double __MarkMWidth = 100;
 		public double MarkMWidth
 		{
 			get => __MarkMWidth;
@@ -226,7 +431,6 @@ namespace TR.BIDSDispX.SampleView
 			}
 		}
 
-		private double __MarkSWidth = 100;
 		public double MarkSWidth
 		{
 			get => __MarkSWidth;
@@ -240,7 +444,6 @@ namespace TR.BIDSDispX.SampleView
 			}
 		}
 
-		private int __MarkLStep = 100;
 		public int MarkLStep
 		{
 			get => __MarkLStep;
@@ -254,7 +457,6 @@ namespace TR.BIDSDispX.SampleView
 			}
 		}
 
-		private int __MarkMStep = 100;
 		public int MarkMStep
 		{
 			get => __MarkMStep;
@@ -268,7 +470,6 @@ namespace TR.BIDSDispX.SampleView
 			}
 		}
 
-		private int __MarkSStep = 100;
 		public int MarkSStep
 		{
 			get => __MarkSStep;
@@ -282,7 +483,6 @@ namespace TR.BIDSDispX.SampleView
 			}
 		}
 
-		private Color __MarkLColor = Color.Black;
 		public Color MarkLColor
 		{
 			get => __MarkLColor;
@@ -296,7 +496,6 @@ namespace TR.BIDSDispX.SampleView
 			}
 		}
 
-		private Color __MarkMColor = Color.Black;
 		public Color MarkMColor
 		{
 			get => __MarkMColor;
@@ -310,7 +509,6 @@ namespace TR.BIDSDispX.SampleView
 			}
 		}
 
-		private Color __MarkSColor = Color.Black;
 		public Color MarkSColor
 		{
 			get => __MarkSColor;
@@ -324,7 +522,60 @@ namespace TR.BIDSDispX.SampleView
 			}
 		}
 
-		private double __Label_Padding = 100;
+		public Thickness MarkLPadding
+		{
+			get => __MarkLPadding;
+			set
+			{
+				if (__MarkLPadding == value)
+					return;
+
+				__MarkLPadding = value;
+				PropCngEvInv(nameof(MarkLPadding));
+			}
+		}
+
+		public Thickness MarkMPadding
+		{
+			get => __MarkMPadding;
+			set
+			{
+				if (__MarkMPadding == value)
+					return;
+
+				__MarkMPadding = value;
+				PropCngEvInv(nameof(MarkMPadding));
+			}
+		}
+
+		public Thickness MarkSPadding
+		{
+			get => __MarkSPadding;
+			set
+			{
+				if (__MarkSPadding == value)
+					return;
+
+				__MarkSPadding = value;
+				PropCngEvInv(nameof(MarkSPadding));
+			}
+		}
+		#endregion
+
+		#region Label Props
+		public bool IsLabelEnabled
+		{
+			get => __IsLabelEnabled;
+			set
+			{
+				if (__IsLabelEnabled == value)
+					return;
+
+				__IsLabelEnabled = value;
+				PropCngEvInv(nameof(IsLabelEnabled));
+			}
+		}
+
 		public double Label_Padding
 		{
 			get => __Label_Padding;
@@ -338,7 +589,6 @@ namespace TR.BIDSDispX.SampleView
 			}
 		}
 
-		private double __Label_FontSize = 100;
 		public double Label_FontSize
 		{
 			get => __Label_FontSize;
@@ -351,6 +601,20 @@ namespace TR.BIDSDispX.SampleView
 				PropCngEvInv(nameof(Label_FontSize));
 			}
 		}
+
+		public int Label_Step
+		{
+			get => __Label_Step;
+			set
+			{
+				if (__Label_Step == value)
+					return;
+
+				__Label_Step = value;
+				PropCngEvInv(nameof(Label_Step));
+			}
+		}
+		#endregion
 		#endregion
 	}
 }
